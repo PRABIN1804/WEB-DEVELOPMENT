556869fbf7486dcd6f293002ccbf47953dd24d0a
PRABIN KUMAR MISHRA <106051007+PRABIN1804@users.noreply.github.com>
2023-01-04 19:48:19 +0530

    files added

---
+++ CSS/DEMO.js
@@ -0,0 +1,12 @@
+// WHAT IS JAVASCRIPT?
+// ->JAVASCRIPT is a client side scripting language. 
+// ->It is used to make web pages .
+// ->It is used to programmatically perform actions within the page. 
+// ->When JAVASCRIPT was created,it was initially  called "LiveScript". 
+// ->But java was a very popular language at that time ,so it was decided that positioning a language as a "younger brother" of java would HTMLTemplateElement. 
+
+
+// WHAT CAN JAVASCRIPT DO?
+// 1.JAVASCRIPT can execute not only in browser, but also on the Server . 
+// 2.We will use JAVASCRIPT as client as well as server side language. 
+// 3.JAVASCRIPT has evolved greatly as a language and is now used to perform wide variety of tasks.
